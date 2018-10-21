47a42c68574f4c0253212126bdc4b2faab8e33c1
rishurajcambrdg <rishurajcambrdg@gmail.com>
2018-10-22 02:13:26 +0530

    Now the program can take inputs from the user

--- take_a_break.py
+++ take_a_break.py
@@ -6,10 +6,13 @@
 import webbrowser
 import time
 
+c = input("Please enter the number of times you want a break: ")
+t = input("How many minute long sessions do you want: ")
+
 count = 0
 print("This program started om" + time.ctime())
-while(count < 3):
-    time.sleep(10)
+while(count < c):
+    time.sleep(t*60)
     webbrowser.open("http://www.youtube.com.")
     print("the time is" + time.ctime())
     count += 1
